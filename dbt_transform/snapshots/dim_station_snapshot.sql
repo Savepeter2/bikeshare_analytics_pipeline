60b6a3e4ff3b1a166f91932dfdafe4035b290dfa
savepeter2 <peteradekolu@gmail.com>
2025-11-13 01:29:24 +0100

    added snapshots for scd2

---
+++ dbt_transform/snapshots/dim_station_snapshot.sql
@@ -0,0 +1,15 @@
+{% snapshot dim_station_snapshot %}
+
+{{
+    config(
+      target_schema='snapshots',
+      unique_key='station_id',
+      strategy='check',
+      check_cols=['station_name'],
+      invalidate_hard_deletes=True
+    )
+}}
+
+SELECT * FROM {{ ref('stg_stations_current') }}
+
+{% endsnapshot %}
